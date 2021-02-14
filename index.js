fde0328fb7cc6c10b0e6909831caf6de543826d1
Muhammad Dzikra Muzaki <mdzkm17@gmail.com>
2021-02-14 19:18:13 +0700

    Update bot

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+const command = require('./command')
+
 client.on('ready', () => {
     console.log('The client is ready!')
+
+    command(client, 'ping', message => {
+        message.channel.send('Pong!')
+    })
 })
 
 client.login(process.env.DISCORD_TOKEN)

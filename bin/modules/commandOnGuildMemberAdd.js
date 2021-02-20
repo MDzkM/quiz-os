f7e86dc36a032a6c84818b3f248944f3cc0184a8
Muhammad Dzikra Muzaki <mdzkm17@gmail.com>
2021-02-20 15:23:44 +0700

    Fix parameters on register

--- bin/modules/commandOnGuildMemberAdd.js
+++ bin/modules/commandOnGuildMemberAdd.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
     client.on('guildMemberAdd', member => {
-        const message = '👋 Hello! I am **Helper OS**, the official guide for **Operating Systems 211** server.\n\nBefore you can participate on any activities, we need to make sure you are registered on this class. I will guide you through the steps and provide necessary information after you are signed up.\n\nTo begin, please send one the following commands on this direct message:```!register GITHUB_ACCOUNT EMAIL CLASS STUDENT_ID ASSISTANT_CODE FULL_NAME```or```!r GITHUB_ACCOUNT EMAIL CLASS STUDENT_ID ASSISTANT_CODE FULL_NAME```\nOnce registered, you will be assigned a nickname and role automatically on the **Operating Systems 211** server.'
+        const message = '👋 Hello! I am **Helper OS**, the official guide for **Operating Systems 211** server.\n\nBefore you can participate on any activities, we need to make sure you are registered on this class. I will guide you through the steps and provide necessary information after you are signed up.\n\nTo begin, please send one the following commands on this direct message: `!register GITHUB_USERNAME EMAIL CLASS STUDENT_ID STUDENT_NAME` or `!r GITHUB_USERNAME EMAIL CLASS STUDENT_ID STUDENT_NAME`\n\nOnce registered, you will be assigned a nickname and role automatically on the **Operating Systems 211** server.'
         member.send(message)
     })
 }
